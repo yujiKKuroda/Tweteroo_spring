c4ef06686c65a88368ada9b7b6bb6ced08ed63a5
Rodrigo Yuji Koga Kuroda <yujikuroda.rykk@gmail.com>
2024-01-29 17:11:05 -0300

    feat: add GET /tweets route

--- api/src/main/java/com/tweteroo/api/controllers/TweetController.java
+++ api/src/main/java/com/tweteroo/api/controllers/TweetController.java
@@ -1,5 +1,30 @@
 package com.tweteroo.api.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tweteroo.api.models.TweetModel;
+import com.tweteroo.api.repositories.TweetRepository;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/tweets")
 public class TweetController {
-  
+
+    final TweetRepository tweetRepository;
+
+		TweetController(TweetRepository tweetRepository) {
+		    this.tweetRepository = tweetRepository;
+		}
+
+    @GetMapping
+    public ResponseEntity<Object> getTweets() {
+        List<TweetModel> tweet = tweetRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(tweet);
+    }
 }
--- api/src/main/java/com/tweteroo/api/repositories/TweetRepository.java
+++ api/src/main/java/com/tweteroo/api/repositories/TweetRepository.java
@@ -1,5 +1,11 @@
 package com.tweteroo.api.repositories;
 
-public class TweetRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tweteroo.api.models.TweetModel;
+
+@Repository
+public interface TweetRepository extends JpaRepository<TweetModel, Long>{
   
 }
