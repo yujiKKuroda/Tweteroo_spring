63cde9d24f17066dda7156f73d4d39c949290414
Rodrigo Yuji Koga Kuroda <yujikuroda.rykk@gmail.com>
2024-01-29 17:11:41 -0300

    feat: add POST /tweets route

--- api/src/main/java/com/tweteroo/api/controllers/TweetController.java
+++ api/src/main/java/com/tweteroo/api/controllers/TweetController.java
@@ -3,14 +3,19 @@ package com.tweteroo.api.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tweteroo.api.dtos.TweetDTO;
 import com.tweteroo.api.models.TweetModel;
 import com.tweteroo.api.repositories.TweetRepository;
+
+import jakarta.validation.Valid;
 
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/tweets")
@@ -21,6 +26,13 @@ public class TweetController {
 		TweetController(TweetRepository tweetRepository) {
 		    this.tweetRepository = tweetRepository;
 		}
+
+    @PostMapping
+    public ResponseEntity<Object> createTweet(@RequestBody @Valid TweetDTO body) {
+        TweetModel tweet = new TweetModel(body);
+        tweetRepository.save(tweet);
+        return ResponseEntity.status(HttpStatus.CREATED).body(tweet);
+    }
 
     @GetMapping
     public ResponseEntity<Object> getTweets() {
