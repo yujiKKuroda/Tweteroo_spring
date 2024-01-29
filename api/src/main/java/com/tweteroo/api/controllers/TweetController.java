ee3f9f6e4e88dbac26c214fe22029fe8b7ef2088
Rodrigo Yuji Koga Kuroda <yujikuroda.rykk@gmail.com>
2024-01-29 17:12:37 -0300

    feat: add GET /tweets/user/:userId route

--- api/src/main/java/com/tweteroo/api/controllers/TweetController.java
+++ api/src/main/java/com/tweteroo/api/controllers/TweetController.java
@@ -10,10 +10,12 @@ import com.tweteroo.api.repositories.TweetRepository;
 import jakarta.validation.Valid;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -38,5 +40,16 @@ public class TweetController {
     public ResponseEntity<Object> getTweets() {
         List<TweetModel> tweet = tweetRepository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(tweet);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Object> getUserById(@PathVariable("id") Long id) {
+        Optional<TweetModel> tweet = tweetRepository.findById(id);
+
+        if (!tweet.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(tweet.get());
     }
 }
