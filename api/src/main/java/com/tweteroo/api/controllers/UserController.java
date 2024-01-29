d217545de495c9035309144eb5c0c300535f4c3b
Rodrigo Yuji Koga Kuroda <yujikuroda.rykk@gmail.com>
2024-01-29 16:51:38 -0300

    feat: add POST /users

--- api/src/main/java/com/tweteroo/api/controllers/UserController.java
+++ api/src/main/java/com/tweteroo/api/controllers/UserController.java
@@ -9,6 +9,8 @@ import com.tweteroo.api.repositories.UserRepository;
 
 import jakarta.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -23,8 +25,9 @@ public class UserController {
 		}
 
     @PostMapping
-    public void createUser(@RequestBody @Valid UserDTO body) {
+    public ResponseEntity<Object> createUser(@RequestBody @Valid UserDTO body) {
         UserModel user = new UserModel(body);
         userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 }
